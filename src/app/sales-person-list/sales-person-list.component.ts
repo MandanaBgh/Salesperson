092c943d1b67993c67b7d65ff122ba98127fc9d1
Baghery  Mandana <Ma_Baghery@iranet.net>
2021-06-13 14:24:13 +0430

    First add

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from '../sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+
+
+  salespersonList : SalesPerson[] =[
+
+    new SalesPerson ("Mandana","Bagheri","Mandana.bgh@gmail.com",40000),
+    new SalesPerson("Mehdi","Yaghoobpour","Mehdi@luv2Code",10000),
+    new SalesPerson("Sosan","Korba","Korba@yahoo.com",4300)
+
+
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
